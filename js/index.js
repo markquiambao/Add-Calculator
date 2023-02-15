cc5c9bc4a37c8a0a2c4ecba1de340024509188f2
markquiambao <110491159+markquiambao@users.noreply.github.com>
2023-02-15 19:42:49 +0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,42 @@
 function addNumbers() {
-        var num1 = parseFloat(document.getElementById("num1").value);
-        var num2 = parseFloat(document.getElementById("num2").value);
-        var num3 = parseFloat(document.getElementById("num3").value);
-        var num4 = parseFloat(document.getElementById("num4").value);
-        var num5 = parseFloat(document.getElementById("num5").value);
-        var num6 = parseFloat(document.getElementById("num6").value);
-        // Check if either input is empty
-        if (isNaN(num1) || isNaN(num2)) {
-          alert("Please input a number");
-          return;
-        }
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var num3 = parseFloat(document.getElementById("num3").value);
+  var num4 = parseFloat(document.getElementById("num4").value);
+  var num5 = parseFloat(document.getElementById("num5").value);
+  var num6 = parseFloat(document.getElementById("num6").value);
+  // Check if either input is empty
+  if (isNaN(num1) || isNaN(num2)) {
+    alert("Please input a number");
+    return;
+  } else if (isNaN(num3) || isNaN(num4) || isNaN(num5) || isNaN(num6)) {
+    document.getElementById("num3").value = "0";
+    document.getElementById("num4").value = "0";
+    document.getElementById("num5").value = "0";
+    document.getElementById("num6").value = "0";
+    return;
+  }
+  var result = num1 + num2 + num3 + num4 + num5 + num6;
+  var result = result.toLocaleString();
+  document.getElementById("result").innerHTML =
+    "<span> Total: </span> " + "  " + result;
+}
+function clearNum1() {
+  document.getElementById("num1").value = "";
+}
 
-        var result = num1 + num2 + num3 + num4 + num5 + num6;
-        var result = result.toLocaleString();
-        document.getElementById("result").innerHTML = "<span> Total: </span> "  +  "  " + result;
-      }
- function clearNum1() {
-        document.getElementById("num1").value = "";
-      }
-
-      function clearNum2() {
-        document.getElementById("num2").value = "";
-      }
+function clearNum2() {
+  document.getElementById("num2").value = "";
+}
 function clearNum3() {
-        document.getElementById("num3").value = "";
-      }
+  document.getElementById("num3").value = "";
+}
 function clearNum4() {
-        document.getElementById("num4").value = "";
-      }
+  document.getElementById("num4").value = "";
+}
 function clearNum5() {
-        document.getElementById("num5").value = "";
-      }
+  document.getElementById("num5").value = "";
+}
 function clearNum6() {
-        document.getElementById("num6").value = "";
-      }
+  document.getElementById("num6").value = "";
+}
